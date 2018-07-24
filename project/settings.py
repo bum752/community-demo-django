0d14860de82cd18b72ff5b5a77eabf0e4b10991d
bum752 <bum752@gmail.com>
2018-07-24 12:23:50 +0900

    heroku deploy

--- project/settings.py
+++ project/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].updae(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
